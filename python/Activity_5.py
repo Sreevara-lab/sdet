fce8f4e0266602b03266dcd19695a13e2ac5afb8
Sreevara-lab <77953871+Sreevara-lab@users.noreply.github.com>
2021-02-08 05:43:32 +0530

    Add files via upload

---
+++ python/Activity_5.py
@@ -0,0 +1,5 @@
+tableNumber = int(input("Enter the number for which you want to see the multiplication Table : "))
+i=1
+while (i < 11):
+    print(tableNumber, ' X ', i, " = ", tableNumber * i)
+    i = i + 1
