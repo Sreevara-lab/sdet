ae3cadac0b46a01cc035b6240fa8e505fe0ad01e
Sreevara-lab <77953871+Sreevara-lab@users.noreply.github.com>
2021-02-14 14:18:15 +0530

    Add files via upload

---
+++ selenium/Activity_2.java
@@ -0,0 +1,44 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+
+public class Activity_2 {
+
+	public static void main(String[] args) {
+		
+/*		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SREEVARAMYSORE\\eclipse-workspace\\chromedriver.exe");
+		WebDriver chromeDriver = new ChromeDriver();
+		chromeDriver.get("https://www.google.com/?gws_rd=ssl");
+		chromeDriver.navigate().to("https://login.microsoftonline.com/common/oauth2/authorize?client_id=00000002-0000-0ff1-ce00-000000000000&redirect_uri=https%3a%2f%2foutlook.office.com%2fowa%2f&resource=00000002-0000-0ff1-ce00-000000000000&response_mode=form_post&response_type=code+id_token&scope=openid&msafed=0&client-request-id=11d5b2be-c5d1-46ef-bafc-5c57d31fa173&protectedtoken=true&claims=%7b%22id_token%22%3a%7b%22xms_cc%22%3a%7b%22values%22%3a%5b%22CP1%22%5d%7d%7d%7d&domain_hint=virginmobileusa.com&nonce=637488823630856620.0324add8-6b4a-4e50-934b-ebbed1923c99&state=DctBDsIgEEBR0Lu4o4wMpbBoPMtMIUoCpam2enxZvL_7Ughx7S6dhB4xOZys996gQ_CjcwYGQGMpRq8cW1I2jaACWlaJOcV7MLiEIPsLun1JP_ZEpc5n3p95rY1zScebhqXV20af16wr5aLzyu33Bw&sso_reload=true");
+		chromeDriver.close();*/
+		
+		WebDriver ffDriver = new FirefoxDriver();
+		ffDriver.get("https://www.training-support.net");
+		String title;
+		WebElement className = ffDriver.findElement(By.className("green"));
+		WebElement idLocator = ffDriver.findElement(By.id("about-link"));
+		WebElement linkText = ffDriver.findElement(By.linkText("About Us"));
+		WebElement cssLocator = ffDriver.findElement(By.cssSelector(".green"));
+		
+		
+		
+		
+		title = ffDriver.getTitle();
+		System.out.println("Page Title is: " + title);
+		System.out.println("Text in the element is : " + className.getText());
+		System.out.println("Text in the element is : " + idLocator.getText());
+		System.out.println("Text in the element is : " + cssLocator.getText());
+		System.out.println("Text in element is : " + linkText.getText());
+		
+		
+		
+		
+
+	}
+
+}
